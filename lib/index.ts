f33ac34dece0126a0cd6bec6b4082d70655727cd
Joshua Hansen <joshlha16@gmail.com>
2023-09-28 14:49:00 -0700

    Remove unused imports

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,3 @@
-import { ZendeskObject } from "./wrapper_types"
-import { Comment } from "./zendesk_types/support_apps/ticket_sidebar"
-
 export { default } from "./reified_client"
 export { Support } from "./support"
 export { Core } from "./core"
